f1d6a3d81eb8932552fd6116e527199687715804
Tachyon Tech Labs <pranjalpchaudhari99@gmail.com>
2019-01-19 03:34:42 +0530

    gradle issue done

--- app/src/main/java/com/tachyon/techlabs/iplauction/PaymentInfo.java
+++ app/src/main/java/com/tachyon/techlabs/iplauction/PaymentInfo.java
@@ -29,7 +29,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.wajahatkarim3.easyflipview.EasyFlipView;
+//import com.wajahatkarim3.easyflipview.EasyFlipView;
 //import com.wajahatkarim3.easyflipview.EasyFlipView;
 
 import java.io.BufferedReader;
